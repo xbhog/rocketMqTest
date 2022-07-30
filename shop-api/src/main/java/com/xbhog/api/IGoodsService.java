4e9d5874ffb9b35d4583d065065cf683a7ea2448
xbhog <2251195014@qq.com>
2022-07-30 15:34:17 +0800

    fix:构建mapper和xml对应文件以及添加各层次依赖

---
+++ shop-api/src/main/java/com/xbhog/api/IGoodsService.java
@@ -0,0 +1,9 @@
+package com.xbhog.api;
+
+/**
+ * @author xbhog
+ * @describe:商品接口
+ * @date 2022/7/30
+ */
+public interface IGoodsService {
+}
--- shop-order-service/src/main/java/com/xbhog/service/impl/OrderServiceImpl.java
+++ shop-order-service/src/main/java/com/xbhog/service/impl/OrderServiceImpl.java
@@ -1,8 +1,14 @@
 package com.xbhog.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.xbhog.api.IGoodsService;
 import com.xbhog.api.IorderService;
+import com.xbhog.constant.ShopCode;
+import com.xbhog.exception.CastException;
 import com.xbhog.shop.entity.Result;
 import com.xbhog.shop.pojo.TradeOrder;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author xbhog
@@ -10,6 +16,10 @@ import com.xbhog.shop.pojo.TradeOrder;
  * @date 2022/7/29
  */
 public class OrderServiceImpl implements IorderService {
+
+    @Reference
+    private IGoodsService iGoodsService;
+
     @Override
     public Result confirmOrder(TradeOrder order) {
         //1.校验订单
@@ -40,6 +50,16 @@ public class OrderServiceImpl implements IorderService {
      * @param order
      */
     private void checkOrder(TradeOrder order) {
+        //1. 校验订单是否存在
+        if(order == null){
+            CastException.cast(ShopCode.SHOP_ORDER_INVALID);
+        }
+        //2.校验订单中的商品是否存在
 
+        //3.校验下单用户是否存在
+
+        //4.校验商品单价是否合法
+
+        //5.校验订单商品数量是否合法
     }
 }
